7c24620df51a63a70ac8317afcbbbe38036ee48e
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-03-17 19:02:13 +0530

    brand page

--- src/CreateBrand.jsx
+++ src/CreateBrand.jsx
@@ -1,52 +1,23 @@
 import { useState, useRef } from "react";
 // import { useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import MDEditor from "@uiw/react-md-editor";
-import "./scss/style.scss";
-import { MdOutlineCancel } from "react-icons/md";
-import { CgAdd } from "react-icons/cg";
-// import IconButton from "@mui/material/IconButton";
+import { useNavigate } from "react-router-dom";
 
 import { create } from "ipfs-http-client";
 
 const client = create("https://ipfs.infura.io:5001/api/v0");
 
-export default function App() {
+export default function CreateBrand() {
   //   const blockchain = useSelector((state) => state.blockchain);
   //   console.log(blockchain);
   // const [ID_ProofFile, setID_ProofFile] = useState(null);
   // const [marksheetFile, setMarksheetFile] = useState(null);
-  const [inputLogo, setInputLogo] = useState([{ name: "", url: "" }]);
-  function addInput() {
-    const values = [...inputLogo];
-    values.push({ name: "", url: "" });
-    setInputLogo(values);
-  }
-  function removeInput(i) {
-    const values = [...inputLogo];
-    values.splice(i, 1);
-    setInputLogo(values);
-  }
-  function handleLogoChange(i, event) {
-    const values = [...inputLogo];
-    values[i].question = event.target.value;
-    setInputLogo(values);
-  }
-  function handleFileChange(i, event) {
-    const values = [...inputLogo];
-    values[i].file = event.target.files[0];
-    setInputLogo(values);
-    // setFile((prev) => [...prev, event.target.files[0]]);
-  }
-
   const [ID_ProofUrl, setID_ProofUrl] = useState("/aadhar-placeholder.jpg");
   const [marksheetUrl, setMarksheetUrl] = useState(
     "/marksheet-placeholder.jpg"
   );
   const [profilePicUrl, setProfilePicUrl] = useState("/avatar.png");
   // const [urlArr, setUrlArr] = useState([]);
-  const [guidlinesValue, setGuidlinesValue] = useState("");
-  let history = useHistory();
+  let history = useNavigate();
 
   const orgNameInputRef = useRef();
   const orgOverviewInputRef = useRef();
@@ -164,23 +135,7 @@ export default function App() {
                 placeholder="Netflix is a streaming service that offers a wide variety of content, including movies, TV shows, anime, documentaries, and more. It is owned by Netflix, Inc., a Delaware corporation."
               />
             </div>
-            <div className="">
-              <label htmlFor="inputOverview" className="text-dark">
-                General Guidlines
-              </label>
-              <MDEditor
-                height={200}
-                value={guidlinesValue}
-                onChange={setGuidlinesValue}
-              />
-              <MDEditor.Markdown
-                source={guidlinesValue}
-                linkTarget="_blank"
-                // previewOptions={{
-                //   linkTarget: "_blank"
-                // }}
-              />
-            </div>
+
             <div className="form-group  my-4">
               <label htmlFor="inputPronounciation" className="text-dark">
                 Pronounciation
@@ -194,59 +149,7 @@ export default function App() {
                 placeholder="li-cet patt-ar-ee"
               />
             </div>
-            <div className="col-md-12">
-              <label htmlFor="inputLogo" className="form-label fs-5 my-4 ">
-                Logo
-              </label>
-              <div className="d-inline-block">
-                <button
-                  sx={{ color: "#182340" }}
-                  aria-label="add"
-                  component="span"
-                  onClick={addInput}
-                >
-                  <CgAdd sx={{ width: "40px", height: "40px" }} />
-                </button>
-              </div>
-              {inputLogo.map((input, index) => (
-                <div key={index}>
-                  <div className="d-flex">
-                    {index > 0 && (
-                      <div className="my-md-2">
-                        <button
-                          onClick={() => removeInput(index)}
-                          className=""
-                          sx={{ color: "#ec523e" }}
-                          aria-label="cancel"
-                          component="span"
-                        >
-                          <MdOutlineCancel
-                            sx={{ width: "40px", height: "40px" }}
-                          />
-                        </button>
-                      </div>
-                    )}
-                    <input
-                      type="text"
-                      className="form-control mb-2 p-2 "
-                      placeholder="Logo"
-                      value={input.name ? input.name : ""}
-                      onChange={(e) => {
-                        handleLogoChange(index, e);
-                      }}
-                    />
-                  </div>
-                  <input
-                    className="form-control p-2 mb-3 "
-                    type="file"
-                    id="logoFile"
-                    onChange={(e) => {
-                      handleFileChange(index, e);
-                    }}
-                  />
-                </div>
-              ))}
-            </div>
+
             <div className="d-flex justify-content-start">
               <div className="me-md-4">
                 <div>Upload Profile Pic</div>
@@ -308,7 +211,7 @@ export default function App() {
             onClick={() => createstudent()}
             className="mt-5 btn d-block btn-lg fw-bold btn-primary p-3"
           >
-            Create Brand ✅
+            Create Student & Proceed ✅
           </div>
         </section>
       </div>
